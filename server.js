c717a49826563cb19ca445afb120133cf719e010
mohamedelkassas93 <python.lover.gm@gmail.com>
2017-03-24 23:25:00 +0200

    Adding features

--- server.js
+++ server.js
@@ -4,20 +4,6 @@
 
 
 
-/*Express Middleware
-
-Middleware can do anything (e.g.):
-    1- logging something to the screen
-    2- make a changr to the request or the response
-    3- to make sure that someone logged in to specify wether he can access specific route(website directory)
-    4- to respond to request useg ( app.send() | app.render() )
-
-**to use Middleware, app.use():
-    app.use( (req, res, next)=>{} )
-    -req & res are the same of app.get() -- no difference
-    - next : when the function execution done
-        if next() not called, the code next to the Middleware wont be executed
-*/
 
 
 const express = require("express");
@@ -31,7 +17,6 @@ const port = process.env.PORT || 3000;
 app.set('view engine', 'hbs');
 app.use("/assets", express.static(__dirname+"/public"));
 
-//[**] using a Middleware
 app.use((req, res, next)=>{
     var now = new Date().getFullYear();
     var log = `${now}: ${req.method} ${req.url}`;
@@ -39,11 +24,7 @@ app.use((req, res, next)=>{
     fs.appendFile("server.log", log + '\n');
     next();
 })
-/*
-app.use( (req, res, next)=>{
-    res.render("maintenance");
-});//with this, any request to any route will return the maintenance.hbs template because we didnt call next() so the down code wont be executed
-*/
+
 hbs.registerPartials(__dirname+"/views/partials");
 
 hbs.registerHelper("getCurrentYear", ()=> {
@@ -72,5 +53,10 @@ app.get('/about', (req, res)=>{
 
 })
 
-
+app.get('/projects', (req, res)=>{
+     res.render("projects", {
+        pageTitle: "Projects Page",
+        welcomeMsg:"Welcome to Our Projects Page"
+    })
+})
 app.listen(port);
